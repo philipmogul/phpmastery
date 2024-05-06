19590ef6249b08e8b2dcb70214ddc1c75aa77743
philipmogul <philipp1mogul@gmail.com>
2024-05-06 22:24:13 +0300

    Git Commands Tutorial File

---
+++ gitbasics.php
@@ -0,0 +1,24 @@
+<?php 
+// git basics 
+
+// configurations = setting username, email 
+// git config --global user.name "PhilipValue"
+// git config --global user.email "philipp1mogul@gmail.com"
+echo "<br/>git config --global user.name = 'Philip Value'   : This command is used to set variables in git config <br/><br/>";
+
+
+// initial commit = git init 
+echo "git init : is used to initialize a commit <br/><br/>";
+
+// make changes = make changes to file on file editor 
+echo "Changes to a file are done on a normal text editor. <br/><br/>";
+
+
+
+// add changes = git add .
+echo "git add . : is used to add files <br/><br/>";
+
+
+
+// commit changes = git commit -m "Message attached to this commit"
+echo "git commit -m 'Messages attached to commit' => This is used to do the final commit. ";
