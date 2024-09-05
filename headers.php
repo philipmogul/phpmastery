df4916bb14bad2e9b6d475e2b088f5502d4f2371
philipmogul <philipp1mogul@gmail.com>
2024-09-05 03:55:34 +0300

    Headers in php

---
+++ headers.php
@@ -0,0 +1,43 @@
+<?php 
+
+// Different types of headers in php 
+header("Content-type: text/plain");
+echo "Hello World!\n";
+echo "PHP programming headers is cool.\n";
+echo "Salud!";
+
+
+// More Header definitions 
+/*
+header("Content-type: application/json");
+$data = ["response"=>"Hello World"];
+echo json_encode($data);
+
+//Output 
+// {"response":"Hello World"}
+
+
+
+header("Content-type: text/html");
+echo "Hello World!";
+
+
+header("Content-type: text/javascript");
+echo "Hello World!";
+
+
+header("Content-type: text/css");
+echo "Hello World!";
+
+
+header("Content-type: multipart/form-data");
+echo "Hello World!";
+
+
+header("Content-type: application/x-www-form-urlencoded");
+echo "Hello World!";
+
+
+*/
+
+
